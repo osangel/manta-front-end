4c3dbe173b99ab8d0b9b05482eae3eb451f68d85
chan <jincan39@163.com>
2023-02-24 18:41:56 +0800

    fix: zkAsset sync tip

--- src/components/ZkAccount/PrivateAssetTableContent.tsx
+++ src/components/ZkAccount/PrivateAssetTableContent.tsx
@@ -3,17 +3,29 @@ import {
   useZkAccountBalances,
   ZkAccountBalance
 } from 'contexts/zkAccountBalancesContext';
+import { useSend } from 'pages/SendPage/SendContext';
 import PrivateAssetItem from './PrivateAssetItem';
 
 const PrivateAssetTableContent = () => {
   const { balances } = useZkAccountBalances();
+  const { senderIsPrivate, receiverIsPrivate } = useSend();
   const privateWallet = usePrivateWallet();
+  const isInitialSync = privateWallet?.isInitialSync?.current;
+  const shouldSyncZKAccount =
+    (isInitialSync && senderIsPrivate()) || (isInitialSync && receiverIsPrivate());
+
   if (balances?.length) {
     return (
       <div className="divide-y divide-dashed divide-manta-gray-secondary">
         {balances.map((balance: ZkAccountBalance) => (
           <PrivateAssetItem balance={balance} key={balance.assetType.assetId} />
         ))}
+      </div>
+    );
+  } else if (shouldSyncZKAccount) {
+    return (
+      <div className="whitespace-nowrap text-center mt-6">
+        Syncing zk account
       </div>
     );
   } else if (privateWallet?.balancesAreStaleRef.current) {
